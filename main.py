38d0f2eba10895fae12db87b80b93a8ee521ba3a
Marius Bock <marius.bock@uni-siegen.de>
2021-12-09 18:24:10 +0100

    Adjusted workflow to new training loop; changed dict init; moved wandb logging to utils; added analysis

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ import os
 import sys
 import time
 
-import numpy as np
-import wandb
+from dl_for_har_analysis.analysis import run_train_analysis, run_test_analysis
+from dl_har_model.models.DeepConvLSTM import DeepConvLSTM
 
-from dl_har_model.train import cross_validate
-from utils import Logger, paint
+from dl_har_model.train import split_validate, loso_cross_validate
+from utils import Logger, wandb_logging
 
-SEEDS = [1, 2, 3, 4, 5]
+SEEDS = [1, 2]
 WANDB_PROJECT = 'grokking_for_har'
 WANDB_ENTITY = 'siegen-sussex-dl-for-har'
 
@@ -76,6 +76,9 @@ def get_args():
         '-bste', '--batch_size_test', type=int, help='Batch size testing. Default 256.',
         default=256, required=False)
     parser.add_argument(
+        '-pf', '--print_freq', type=int, help='Print frequency (batches). Default 256.',
+        default=100, required=False)
+    parser.add_argument(
         '--wandb', action='store_true', help='Flag indicating to log results to wandb.',
         default=False, required=False)
     parser.add_argument(
@@ -95,63 +98,52 @@ def get_args():
 
 args = get_args()
 
-# general settings
-wandb_logging = args.wandb
-log_results = args.logging
-save_results = args.save_results
+config_dataset = {
+    "dataset": args.dataset,
+    "window": args.window_size,
+    "stride": args.window_step_train,
+    "stride_test": args.window_step_test,
+    "path_processed": f"data/{args.dataset}",
+}
 
-# dataset settings
-target_dataset = args.dataset
-valid_type = args.valid_type
-window_size = args.window_size
-window_step_train = args.window_step_train
-window_step_test = args.window_step_test
-
-
-# training settings
-model = args.model
-batch_size_train = args.batch_size_train
-optimizer = args.optimizer
-loss = args.loss
-smoothing = args.smoothing
-use_weights = args.unweighted
-lr = args.learning_rate
-lr_schedule = args.learning_rate_schedule
-lr_step = args.learning_rate_schedule_step
-lr_decay = args.learning_rate_schedule_decay
-weights_init = args.weights_init
-weight_decay = args.weight_decay
-epochs = args.n_epochs
-
-# testing settings
-batch_size_test = args.batch_size_test
+train_args = {
+    "batch_size_train": args.batch_size_train,
+    "batch_size_test": args.batch_size_test,
+    "optimizer": args.optimizer,
+    "use_weights": args.unweighted,
+    "lr": args.learning_rate,
+    "lr_schedule": args.learning_rate_schedule,
+    "lr_step": args.learning_rate_schedule_step,
+    "lr_decay": args.weight_decay,
+    "weights_init": args.weights_init,
+    "epochs": args.n_epochs,
+    "print_freq": args.print_freq,
+    "loss": args.loss,
+    "smoothing": args.smoothing,
+    "weight_decay": args.weight_decay
+}
 
 config = dict(
     seeds=SEEDS,
-    dataset=target_dataset,
-    model=model,
-    valid_type=valid_type,
-    window=window_size,
-    stride=window_step_train,
-    batch_size_train=batch_size_train,
-    epochs=epochs,
-    optimizer=optimizer,
-    loss=loss,
-    smoothing=smoothing,
-    use_weights=use_weights,
-    lr=lr,
-    lr_schedule=lr_schedule,
-    lr_step=lr_step,
-    lr_decay=lr_decay,
-    weights_init=weights_init,
-    weight_decay=weight_decay,
-    batch_size_test=batch_size_test,
-    stride_test=window_step_test,
-    wandb_logging=wandb_logging
+    model=args.model,
+    valid_type=args.valid_type,
+    batch_size_train=args.batch_size_train,
+    epochs=args.n_epochs,
+    optimizer=args.optimizer,
+    loss=args.loss,
+    smoothing=args.smoothing,
+    use_weights=args.unweighted,
+    lr=args.learning_rate,
+    lr_schedule=args.learning_rate_schedule,
+    lr_step=args.learning_rate_schedule_step,
+    lr_decay=args.learning_rate_schedule_decay,
+    weights_init=args.weights_init,
+    weight_decay=args.weight_decay,
+    batch_size_test=args.batch_size_test,
+    wandb_logging=args.wandb
 )
 
 # parameters used to calculate runtime
-start = time.time()
 log_date = time.strftime('%Y%m%d')
 log_timestamp = time.strftime('%H%M%S')
 
@@ -159,57 +151,22 @@ log_timestamp = time.strftime('%H%M%S')
 if args.logging:
     sys.stdout = Logger(os.path.join('logs', log_date, log_timestamp, 'log'))
 
-if wandb_logging:
-    wandb.init(project=WANDB_PROJECT, entity=WANDB_ENTITY, config=config)
+# TODO: model declaration with importlib
+model = DeepConvLSTM(113, 18, 'opportunity').cuda()
 
-train_results, test_results, preds = cross_validate(valid_type, config, verbose=True)
+if args.valid_type == 'split':
+    train_results, test_results, preds = split_validate(model, train_args, config_dataset, SEEDS, verbose=True)
+elif args.valid_type == 'loso':
+    train_results, test_results, preds = loso_cross_validate(model, train_args, config_dataset, SEEDS, verbose=True)
 
-if wandb_logging:
-    t_loss, t_acc, t_fw, t_fm = np.zeros(epochs), np.zeros(epochs), np.zeros(epochs), np.zeros(epochs)
-    v_loss, v_acc, v_fw, v_fm = np.zeros(epochs), np.zeros(epochs), np.zeros(epochs), np.zeros(epochs)
+run_train_analysis(train_results)
+run_test_analysis(test_results)
 
-    for i in range(len(train_results)):
-        t_loss = np.add(t_loss, train_results['t_loss'][i])
-        t_acc = np.add(t_acc, train_results['t_acc'][i])
-        t_fw = np.add(t_fw, train_results['t_fw'][i])
-        t_fm = np.add(t_fm, train_results['t_fm'][i])
+if args.wandb:
+    wandb_logging(train_results, test_results, WANDB_PROJECT, WANDB_ENTITY, {**config_dataset, **train_args})
 
-        v_loss = np.add(v_loss, train_results['v_loss'][i])
-        v_acc = np.add(v_acc, train_results['v_acc'][i])
-        v_fw = np.add(v_fw, train_results['v_fw'][i])
-        v_fm = np.add(v_fm, train_results['v_fm'][i])
-
-    table = wandb.Table(data=[[a, b, c, d, e, f, g, h, i] for (a, b, c, d, e, f, g, h, i) in
-                              zip(list(range(epochs)), t_loss / len(train_results), t_acc / len(train_results),
-                                  t_fw / len(train_results), t_fm / len(train_results), v_loss / len(train_results),
-                                  v_acc / len(train_results), t_fw / len(train_results), t_fm / len(train_results))],
-                        columns=["epochs", "t_loss", "t_acc", "t_fw", "t_fm", "v_loss", "v_acc", "f_fw", "f_fm"]
-                        )
-
-    wandb.log({"train_loss": wandb.plot.line(table, "epochs", "t_loss", title='Train Loss'),
-               "train_acc": wandb.plot.line(table, "epochs", "t_acc", title='Train Accuracy'),
-               "train_fm": wandb.plot.line(table, "epochs", "t_fm", title='Train F1-macro'),
-               "train_fw": wandb.plot.line(table, "epochs", "t_fw", title='Train F1-weighted'),
-               "val_loss": wandb.plot.line(table, "epochs", "v_loss", title='Valid Loss'),
-               "val_acc": wandb.plot.line(table, "epochs", "v_acc", title='Valid Accuracy'),
-               "val_fm": wandb.plot.line(table, "epochs", "v_fm", title='Valid F1-macro'),
-               "val_fw": wandb.plot.line(table, "epochs", "v_fw", title='Valid F1-weigthed')})
-
-    if test_results is not None:
-        wandb.log({"test_loss": test_results['test_loss'].mean(),
-                   "test_acc": test_results['test_acc'].mean(),
-                   "test_fm": test_results['test_fm'].mean(),
-                   "test_fw": test_results['test_fw'].mean()
-                   })
-
-if save_results:
+if args.save_results:
     train_results.to_csv(os.path.join('logs', log_date, log_timestamp, 'train_results.csv'), index=False)
     if test_results is not None:
         test_results.to_csv(os.path.join('logs', log_date, log_timestamp, 'test_results.csv'), index=False)
     preds.to_csv(os.path.join('logs', log_date, log_timestamp, 'preds.csv'), index=False)
-
-# calculate time data creation took
-end = time.time()
-hours, rem = divmod(end - start, 3600)
-minutes, seconds = divmod(rem, 60)
-print(paint("\nFinal time elapsed: {:0>2}:{:0>2}:{:05.2f}\n".format(int(hours), int(minutes), seconds)))
